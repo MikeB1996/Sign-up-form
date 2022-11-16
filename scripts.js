2bfcde6c5ca3e377f18abc85a860ea908937bdea
MikeB1996 <mikelbarko2@gmail.com>
2022-11-16 18:54:48 +0100

    some cleaning in the css

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ const loginBut= document.getElementById("subBut");
 const formVar=document.querySelector("form");
 formVar.addEventListener("submit", checkEqual);
 
-checkEqual(form){
+
+//this is not working right now
+function checkEqual(form){
     pw1=form.passwordBox;
     pw2=form.passwordConfirm;
     if (password1 != password2) {
-        alert ("\nPassword did not match: Please try again...")
+        alert ("\nPassword did not match: Please try again...");
         return false;
     } else {
         alert("password match");
